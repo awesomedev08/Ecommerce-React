4e1a7d32fcae6d21797b1d8ba0b528cab230d91f
awesomedev08 <awesomedev08@gmail.com>
2023-07-11 14:57:55 +0200

    updata Search

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -4,21 +4,24 @@ import style from "./Search.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import ProductGroup from "../../components/productGroup/ProductGroup";
+import PaginationControlled from "../../components/PaginationControlled/PaginationControlled";
 function Search() {
   const { keyword } = useParams();
   const [Mydata, setMyData] = useState([]);
   const [loading, setloading] = useState(false);
-
+  // Pagination
+  const [Pagination, setPagination] = useState(1);
+  // ==Pagination==
   useEffect(() => {
     setloading(false);
     // console.log(Paramsapi);
     axios
       .get(
-        `${process.env.REACT_APP_URL_API}prodects?populate=*&filters[$or][0][desc][$containsi]=${keyword}&filters[$or][1][name][$containsi]=${keyword}`
+        `${process.env.REACT_APP_URL_API}prodects?populate=*&filters[$or][0][desc][$containsi]=${keyword}&filters[$or][1][name][$containsi]=${keyword}&pagination[page]=${Pagination}`
       )
       .then(function (response) {
         //  console.log(response.data.data);
-        setMyData(response.data.data);
+        setMyData(response.data);
       })
       .then(() => {
         setloading(true);
@@ -27,14 +30,15 @@ function Search() {
       .catch(function (error) {
         console.log(error);
       });
-  }, [keyword]);
+  }, [keyword, Pagination]);
   return (
     <div className="Search container">
       <div className="SearchText">
         Search : <span>{keyword}</span>
       </div>
 
-      <ProductGroup data={Mydata} doneLoading={loading} />
+      <ProductGroup data={Mydata.data} doneLoading={loading} />
+      <PaginationControlled Mydata={Mydata} setPagination={setPagination} />
     </div>
   );
 }
